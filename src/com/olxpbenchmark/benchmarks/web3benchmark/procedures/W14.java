f4493016c13fa6686857ddb985602a4efdbc2f50
Gzx@151 <gezhongxin18@mails.ucas.ac.cn>
2023-12-15 18:25:37 +0800

    format the project using vscode W13 W14

--- src/com/olxpbenchmark/benchmarks/web3benchmark/procedures/W14.java
+++ src/com/olxpbenchmark/benchmarks/web3benchmark/procedures/W14.java
@@ -33,57 +33,57 @@ import com.olxpbenchmark.benchmarks.web3benchmark.WEB3Worker;
 
 public class W14 extends WEB3Procedure {
 
-        private static final Logger LOG = Logger.getLogger(W14.class);
+    private static final Logger LOG = Logger.getLogger(W14.class);
 
-        public SQLStmt query_stmtSQL = new SQLStmt(
-                        "insert into "
-                                        + "token_transfers "
-                                        + "values "
-                                        + "(?, ?, ?, ?, ?, ?, ?)");
+    public SQLStmt query_stmtSQL = new SQLStmt(
+            "insert into "
+                    + "token_transfers "
+                    + "values "
+                    + "(?, ?, ?, ?, ?, ?, ?)");
 
-        private PreparedStatement query_stmt = null;
+    private PreparedStatement query_stmt = null;
 
-        public ResultSet run(Connection conn, Random gen, WEB3Worker w, int startNumber, int upperLimit, int numScale,
-                        String nodeid) throws SQLException {
-                boolean trace = LOG.isTraceEnabled();
+    public ResultSet run(Connection conn, Random gen, WEB3Worker w, int startNumber, int upperLimit, int numScale,
+            String nodeid) throws SQLException {
+        boolean trace = LOG.isTraceEnabled();
 
-                // initializing all prepared statements
-                query_stmt = this.getPreparedStatement(conn, query_stmtSQL);
+        // initializing all prepared statements
+        query_stmt = this.getPreparedStatement(conn, query_stmtSQL);
 
-                String token_address = WEB3Util
-                                .convertToTokenAddressString(
-                                                WEB3Util.randomNumber(1, WEB3Config.configTokenCount, gen));
-                String from_address = WEB3Util
-                                .convertToAddressString(WEB3Util.randomNumber(1, WEB3Config.configAccountsCount, gen));
-                String to_address = WEB3Util
-                                .convertToAddressString(WEB3Util.randomNumber(1, WEB3Config.configAccountsCount, gen));
-                double value = (double) WEB3Util.randomNumber(0, 1000000, gen);
-                String transaction_hash = WEB3Util.convertToTxnHashString(
-                                WEB3Util.randomNumber(1, numScale * WEB3Config.configTransactionsCount, gen));
-                long block_number = WEB3Util.randomNumber(1, numScale * WEB3Config.configBlocksCount, gen);
-                long next_block_number = block_number + 1;
+        String token_address = WEB3Util
+                .convertToTokenAddressString(
+                        WEB3Util.randomNumber(1, WEB3Config.configTokenCount, gen));
+        String from_address = WEB3Util
+                .convertToAddressString(WEB3Util.randomNumber(1, WEB3Config.configAccountsCount, gen));
+        String to_address = WEB3Util
+                .convertToAddressString(WEB3Util.randomNumber(1, WEB3Config.configAccountsCount, gen));
+        double value = (double) WEB3Util.randomNumber(0, 1000000, gen);
+        String transaction_hash = WEB3Util.convertToTxnHashString(
+                WEB3Util.randomNumber(1, numScale * WEB3Config.configTransactionsCount, gen));
+        long block_number = WEB3Util.randomNumber(1, numScale * WEB3Config.configBlocksCount, gen);
+        long next_block_number = block_number + 1;
 
-                int idx = 1;
-                query_stmt.setString(idx++, token_address);
-                query_stmt.setString(idx++, from_address);
-                query_stmt.setString(idx++, to_address);
-                query_stmt.setDouble(idx++, value);
-                query_stmt.setString(idx++, transaction_hash);
-                query_stmt.setLong(idx++, block_number);
-                query_stmt.setLong(idx++, next_block_number);
+        int idx = 1;
+        query_stmt.setString(idx++, token_address);
+        query_stmt.setString(idx++, from_address);
+        query_stmt.setString(idx++, to_address);
+        query_stmt.setDouble(idx++, value);
+        query_stmt.setString(idx++, transaction_hash);
+        query_stmt.setLong(idx++, block_number);
+        query_stmt.setLong(idx++, next_block_number);
 
-                if (trace)
-                        LOG.trace("query_stmt W14 InsertTokenTransfers START");
-                int affectedRows = query_stmt.executeUpdate();
-                conn.commit();
-                if (trace)
-                        LOG.trace("query_stmt W14 InsertTokenTransfers END");
+        if (trace)
+            LOG.trace("query_stmt W14 InsertTokenTransfers START");
+        int affectedRows = query_stmt.executeUpdate();
+        conn.commit();
+        if (trace)
+            LOG.trace("query_stmt W14 InsertTokenTransfers END");
 
-                if (LOG.isDebugEnabled()) {
-                        LOG.debug(queryToString(query_stmt));
-                        LOG.debug("W14 InsertTokenTransfers: " + affectedRows + " rows affected");
-                }
-
-                return null;
+        if (LOG.isDebugEnabled()) {
+            LOG.debug(queryToString(query_stmt));
+            LOG.debug("W14 InsertTokenTransfers: " + affectedRows + " rows affected");
         }
+
+        return null;
+    }
 }
